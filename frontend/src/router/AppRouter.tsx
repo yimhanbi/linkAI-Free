d152e690c989c945d085209d745dfb10462a9e53
yimhanbi <dlagksql4@gmail.com>
2026-02-05 16:54:24 +0900

    Revert "chore: route LinkAI logo to welcome page"
    This reverts commit 4c328ed0b3ec188cd8b9961b3e9db52b61630d4b.

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from "react";
 import MainLayout from "../View/layout/MainLayout";
 import AdvancedSearchPage from "../View/advancedSearch/AdvancedSearchPage";
 import LoginPage from "@/View/auth/LoginPage";
-import WelcomePage from "@/View/home/WelcomePage";
 
 export default function AppRouter() {
   // 1. 로컬 스토리지에서 토큰 확인 (로그인 여부) - 상태로 관리하여 변경 감지
@@ -35,7 +34,7 @@ export default function AppRouter() {
       {/* 기본 접속(/) 시: 로그인 여부에 따라 리다이렉트 */}
       <Route 
         path="/" 
-        element={isLoggedIn ? <Navigate to="/welcome" replace /> : <Navigate to="/login" replace />} 
+        element={isLoggedIn ? <Navigate to="/chatbot" replace /> : <Navigate to="/login" replace />} 
       />
 
       {/* 로그인 페이지: 레이아웃 없이 단독으로 보여야 함 */}
@@ -46,8 +45,6 @@ export default function AppRouter() {
 
       {/* 메인 서비스 영역: 로그인한 경우에만 접근 가능 (MainLayout 적용) */}
       <Route element={isLoggedIn ? <MainLayout /> : <Navigate to="/login" replace />}>
-        {/* 웰컴 페이지 */}
-        <Route path="/welcome" element={<WelcomePage />} />
         {/* 상세 검색 페이지 */}
         <Route path="/advanced-search" element={<AdvancedSearchPage />} />
         {/* 챗봇 페이지 (실제 UI는 MainLayout의 ChatbotContainer가 담당) */}
